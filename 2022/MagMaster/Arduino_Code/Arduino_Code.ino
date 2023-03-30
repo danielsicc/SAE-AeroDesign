577be82549e892ee061f36a9a69f7ce0e1b84967
danielsicc <92346192+danielsicc@users.noreply.github.com>
2023-03-29 19:44:46 -0700

    Add files via upload

---
+++ 2022/MagMaster/Arduino_Code/Arduino_Code.ino
@@ -0,0 +1,98 @@
+#include "Wire.h"
+#include "HMC5883L.h"
+
+HMC5883L compass; //Copy the folder "HMC5883L" in the folder "C:\Program Files\Arduino\libraries" and restart the arduino IDE.
+
+float xv, yv, zv;
+
+void setup()
+{   
+  Serial.begin(9600);
+  Wire.begin();  
+  // Initialize HMC5883L
+  Serial.println("Initialize HMC5883L");
+  while (!compass.begin())
+  {
+    Serial.println("Could not find a valid HMC5883L sensor, check wiring!");
+    delay(500);
+  }
+  compass.setRange(HMC5883L_RANGE_1_3GA);
+  compass.setMeasurementMode(HMC5883L_CONTINOUS);
+  compass.setDataRate(HMC5883L_DATARATE_15HZ);
+  compass.setSamples(HMC5883L_SAMPLES_1);
+  checkSettings();     
+}
+
+void checkSettings()
+{
+  Serial.print("Selected range: ");
+  
+  switch (compass.getRange())
+  {
+    case HMC5883L_RANGE_0_88GA: Serial.println("0.88 Ga"); break;
+    case HMC5883L_RANGE_1_3GA:  Serial.println("1.3 Ga"); break;
+    case HMC5883L_RANGE_1_9GA:  Serial.println("1.9 Ga"); break;
+    case HMC5883L_RANGE_2_5GA:  Serial.println("2.5 Ga"); break;
+    case HMC5883L_RANGE_4GA:    Serial.println("4 Ga"); break;
+    case HMC5883L_RANGE_4_7GA:  Serial.println("4.7 Ga"); break;
+    case HMC5883L_RANGE_5_6GA:  Serial.println("5.6 Ga"); break;
+    case HMC5883L_RANGE_8_1GA:  Serial.println("8.1 Ga"); break;
+    default: Serial.println("Bad range!");
+  }
+  
+  Serial.print("Selected Measurement Mode: ");
+  switch (compass.getMeasurementMode())
+  {  
+    case HMC5883L_IDLE: Serial.println("Idle mode"); break;
+    case HMC5883L_SINGLE:  Serial.println("Single-Measurement"); break;
+    case HMC5883L_CONTINOUS:  Serial.println("Continuous-Measurement"); break;
+    default: Serial.println("Bad mode!");
+  }
+
+  Serial.print("Selected Data Rate: ");
+  switch (compass.getDataRate())
+  {  
+    case HMC5883L_DATARATE_0_75_HZ: Serial.println("0.75 Hz"); break;
+    case HMC5883L_DATARATE_1_5HZ:  Serial.println("1.5 Hz"); break;
+    case HMC5883L_DATARATE_3HZ:  Serial.println("3 Hz"); break;
+    case HMC5883L_DATARATE_7_5HZ: Serial.println("7.5 Hz"); break;
+    case HMC5883L_DATARATE_15HZ:  Serial.println("15 Hz"); break;
+    case HMC5883L_DATARATE_30HZ: Serial.println("30 Hz"); break;
+    case HMC5883L_DATARATE_75HZ:  Serial.println("75 Hz"); break;
+    default: Serial.println("Bad data rate!");
+  }
+  
+  Serial.print("Selected number of samples: ");
+  switch (compass.getSamples())
+  {  
+    case HMC5883L_SAMPLES_1: Serial.println("1"); break;
+    case HMC5883L_SAMPLES_2: Serial.println("2"); break;
+    case HMC5883L_SAMPLES_4: Serial.println("4"); break;
+    case HMC5883L_SAMPLES_8: Serial.println("8"); break;
+    default: Serial.println("Bad number of samples!");
+  }
+
+}
+
+void loop()
+{
+  getHeading();
+
+  Serial.flush(); 
+  Serial.print(xv); 
+  Serial.print(",");
+  Serial.print(yv);
+  Serial.print(",");
+  Serial.print(zv);
+  Serial.println();
+
+  delay(100); 
+} 
+ 
+void getHeading()
+{ 
+  Vector raw = compass.readRaw();
+  xv=raw.XAxis;
+  yv=raw.YAxis;
+  zv=raw.ZAxis;
+}
---
+++ 2022/MagMaster/Compass_Heading_Modificado/Compass_Heading_Modificado.ino
@@ -0,0 +1,209 @@
+#include "Wire.h"
+#include "HMC5883L.h"
+#include "BMP085.h"
+
+HMC5883L compass;
+BMP085 bmp;
+
+double referencePressure;
+float xv, yv, zv;
+float calibrated_values[3];
+
+void transformation(float uncalibrated_values[3])    
+{
+  //calibration_matrix[3][3] is the transformation matrix
+  //replace M11, M12,..,M33 with your transformation matrix data
+  double calibration_matrix[3][3] = 
+  {
+    {1.33,0.021,-0.243},
+    {0.032,1.366,0.017},
+    {0.077,-0.081,1.471}  
+  };
+  //bias[3] is the bias
+  //replace Bx, By, Bz with your bias data
+  double bias[3] = 
+  {
+    -22.891,
+    -109.252,
+    -248.098
+  };  
+  //calculation
+  for (int i=0; i<3; ++i) uncalibrated_values[i] = uncalibrated_values[i] - bias[i];
+  float result[3] = {0, 0, 0};
+  for (int i=0; i<3; ++i)
+    for (int j=0; j<3; ++j)
+      result[i] += calibration_matrix[i][j] * uncalibrated_values[j];
+  for (int i=0; i<3; ++i) calibrated_values[i] = result[i];
+}
+
+//vector_length_stabilasation() - is the function of the magnetometer vector length stabilasation (stabilisation of the sphere radius)
+float scaler;
+boolean scaler_flag = false;
+float normal_vector_length;
+void vector_length_stabilasation(){
+  //calculate the normal vector length
+  if (scaler_flag == false)
+  {
+    getHeading();
+    normal_vector_length = sqrt(calibrated_values[0]*calibrated_values[0] + calibrated_values[1]*calibrated_values[1] + calibrated_values[2]*calibrated_values[2]);
+    scaler_flag = true;
+  } 
+  //calculate the current scaler
+  scaler = normal_vector_length/sqrt(calibrated_values[0]*calibrated_values[0] + calibrated_values[1]*calibrated_values[1] + calibrated_values[2]*calibrated_values[2]);
+  //apply the current scaler to the calibrated coordinates (global array calibrated_values)
+  calibrated_values[0] = calibrated_values[0]*scaler;
+  calibrated_values[1] = calibrated_values[1]*scaler;
+  calibrated_values[2] = calibrated_values[2]*scaler;
+}
+
+void setup()
+{   
+  Serial.begin(9600);
+  Wire.begin();  
+  // Initialize HMC5883L
+  Serial.println("Initialize HMC5883L");
+  while (!compass.begin())
+  {
+    Serial.println("Could not find a valid HMC5883L sensor, check wiring!");
+    delay(500);
+  }
+  compass.setRange(HMC5883L_RANGE_1_3GA);
+  compass.setMeasurementMode(HMC5883L_CONTINOUS);
+  compass.setDataRate(HMC5883L_DATARATE_30HZ);
+  compass.setSamples(HMC5883L_SAMPLES_8);
+  checkSettings();
+
+  Serial.println("Initialize BMP085/BMP180 Serial");
+  while(!bmp.begin(BMP085_HIGH_RES))
+  {
+    Serial.println("Could not find a valid BMP085 or BMP180 sensor, check wiring!");
+    delay(500);
+  }
+  // Get reference pressure for relative altitude
+  referencePressure = 101400;
+}
+
+void checkSettings()
+{
+  Serial.print("Selected range: ");
+  
+  switch (compass.getRange())
+  {
+    case HMC5883L_RANGE_0_88GA: Serial.println("0.88 Ga"); break;
+    case HMC5883L_RANGE_1_3GA:  Serial.println("1.3 Ga"); break;
+    case HMC5883L_RANGE_1_9GA:  Serial.println("1.9 Ga"); break;
+    case HMC5883L_RANGE_2_5GA:  Serial.println("2.5 Ga"); break;
+    case HMC5883L_RANGE_4GA:    Serial.println("4 Ga"); break;
+    case HMC5883L_RANGE_4_7GA:  Serial.println("4.7 Ga"); break;
+    case HMC5883L_RANGE_5_6GA:  Serial.println("5.6 Ga"); break;
+    case HMC5883L_RANGE_8_1GA:  Serial.println("8.1 Ga"); break;
+    default: Serial.println("Bad range!");
+  }
+  
+  Serial.print("Selected Measurement Mode: ");
+  switch (compass.getMeasurementMode())
+  {  
+    case HMC5883L_IDLE: Serial.println("Idle mode"); break;
+    case HMC5883L_SINGLE:  Serial.println("Single-Measurement"); break;
+    case HMC5883L_CONTINOUS:  Serial.println("Continuous-Measurement"); break;
+    default: Serial.println("Bad mode!");
+  }
+
+  Serial.print("Selected Data Rate: ");
+  switch (compass.getDataRate())
+  {  
+    case HMC5883L_DATARATE_0_75_HZ: Serial.println("0.75 Hz"); break;
+    case HMC5883L_DATARATE_1_5HZ:  Serial.println("1.5 Hz"); break;
+    case HMC5883L_DATARATE_3HZ:  Serial.println("3 Hz"); break;
+    case HMC5883L_DATARATE_7_5HZ: Serial.println("7.5 Hz"); break;
+    case HMC5883L_DATARATE_15HZ:  Serial.println("15 Hz"); break;
+    case HMC5883L_DATARATE_30HZ: Serial.println("30 Hz"); break;
+    case HMC5883L_DATARATE_75HZ:  Serial.println("75 Hz"); break;
+    default: Serial.println("Bad data rate!");
+  }
+  
+  Serial.print("Selected number of samples: ");
+  switch (compass.getSamples())
+  {  
+    case HMC5883L_SAMPLES_1: Serial.println("1"); break;
+    case HMC5883L_SAMPLES_2: Serial.println("2"); break;
+    case HMC5883L_SAMPLES_4: Serial.println("4"); break;
+    case HMC5883L_SAMPLES_8: Serial.println("8"); break;
+    default: Serial.println("Bad number of samples!");
+  }
+  long bmpVersion = bmp.getVersion();
+  Serial.print("Chip version: ");
+  Serial.print(bmpVersion >> 8); Serial.print("."); Serial.print(bmpVersion & 0xFF);
+  Serial.print(" (0x"); Serial.print(bmpVersion, HEX); Serial.println(")");
+
+  Serial.print("Oversampling: ");
+  Serial.println(bmp.getOversampling());
+  Serial.print("Software Oversampling: ");
+  Serial.println(bmp.getSoftwareOversampling());
+}
+
+void loop()
+{
+  float values_from_magnetometer[3];
+  
+  getHeading();
+  values_from_magnetometer[0] = xv;
+  values_from_magnetometer[1] = yv;
+  values_from_magnetometer[2] = zv;
+  transformation(values_from_magnetometer);
+  
+  vector_length_stabilasation();
+
+  float heading = atan2(calibrated_values[1],calibrated_values[0]); 
+
+  float declinationAngle = (10 + (45.0 / 60.0)) / (180 / M_PI);
+  heading += declinationAngle - (PI/2);
+
+  // Correct for heading < 0deg and heading > 360deg
+  if (heading < 0)
+  {
+    heading += 2 * PI;
+  }
+
+  if (heading > 2 * PI)
+  {
+    heading -= 2 * PI;
+  }
+
+  // Convert to degrees
+  float headingDegrees = heading * 180/M_PI; 
+
+  // Output
+  Serial.print(" Heading = ");
+  Serial.print(heading);
+  Serial.print(" Degress = ");
+  Serial.print(headingDegrees);
+  Serial.print("  ");
+
+  // Read true temperature & Pressure
+  double realTemperature = bmp.readTemperature();
+  long realPressure = bmp.readPressure();
+  // Calculate altitude
+  float absoluteAltitude = bmp.getAltitude(realPressure);
+  float relativeAltitude = bmp.getAltitude(realPressure, referencePressure);
+
+  Serial.print(", realTemp = ");
+  Serial.print(realTemperature);
+  Serial.print(" *C");
+  Serial.print(", realPressure = ");
+  Serial.print(realPressure);
+  Serial.print(" Pa");
+  Serial.print(" absoluteAltitude = ");
+  Serial.print(absoluteAltitude);
+  Serial.print(" m, relativeAltitude = ");
+  Serial.print(relativeAltitude);    
+  Serial.println(" m");
+} 
+ 
+void getHeading()
+{ 
+  Vector raw = compass.readRaw();
+  xv=raw.XAxis;
+  yv=raw.YAxis;
+  zv=raw.ZAxis;
+}
